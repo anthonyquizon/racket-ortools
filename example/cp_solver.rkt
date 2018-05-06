970053344cc39483eb4e190d66c4afc143485e30
Anthony Quizon <anthoq88@gmail.com>
2018-05-07 09:11:04 +1000

    example setup

--- example/cp_solver.rkt
+++ example/cp_solver.rkt
@@ -1,4 +1,32 @@
 #lang racket
 
+(require "../private/ffi.rkt")
+
+(define numVals 3)
+(define solver (solver_new "CPSimple"))
+(define x (solver_MakeIntVar solver 0 (- numVals 1) "x"))
+(define y (solver_MakeIntVar solver 0 (- numVals 1) "y"))
+(define z (solver_MakeIntVar solver 0 (- numVals 1) "z"))
+
+(define xyvars `(,x ,y))
+(define allvars `(,x ,y ,z))
+
+(solver_AddConstraint solver (solver_MakeAllDifferent solver xyvars (length xyvars)))
+
+(define db (solver_MakePhase solver allvars (length allvars) 'CHOOSE_FIRST_UNBOUND 'ASSIGN_MIN_VALUE))
+
+(solver_Solve solver db)
+
+(for ([n (in-naturals)]
+      #:break (not (solver_NextSolution solver db)))
+  (displayln 
+    (format 
+      "Solution ~a:\n x=~a\n y=~a\n z=~a\n" 
+      n 
+      (value_IntVar x)
+      (value_IntVar y)
+      (value_IntVar z))))
+
+
 
 
--- private/ffi.rkt
+++ private/ffi.rkt
@@ -1,9 +1,19 @@
 #lang racket
 
+(provide solver_new
+         solver_delete
+         solver_MakeIntVar
+         solver_AddConstraint
+         solver_MakePhase
+         solver_MakeAllDifferent
+         solver_Solve
+         solver_NextSolution
+         value_IntVar)
+
 (require ffi/unsafe
          ffi/unsafe/define
          ffi/unsafe/alloc)
-
+         
 (define-ffi-definer define-ortools (ffi-lib "./extern/libcwrapper"))
 
 (define _SOLVER-pointer (_cpointer 'SOLVER))
@@ -51,30 +61,3 @@
 (define-ortools solver_Solve (_fun _SOLVER-pointer _DECISION_BUILDER-pointer -> _bool))
 (define-ortools solver_NextSolution (_fun _SOLVER-pointer _DECISION_BUILDER-pointer -> _bool))
 (define-ortools value_IntVar (_fun _INTVAR-pointer -> _int))
-
-
-(define numVals 3)
-(define solver (solver_new "CPSimple"))
-(define x (solver_MakeIntVar solver 0 (- numVals 1) "x"))
-(define y (solver_MakeIntVar solver 0 (- numVals 1) "y"))
-(define z (solver_MakeIntVar solver 0 (- numVals 1) "z"))
-
-(define xyvars `(,x ,y))
-(define allvars `(,x ,y ,z))
-
-(solver_AddConstraint solver (solver_MakeAllDifferent solver xyvars (length xyvars)))
-
-(define db (solver_MakePhase solver allvars (length allvars) 'CHOOSE_FIRST_UNBOUND 'ASSIGN_MIN_VALUE))
-
-(solver_Solve solver db)
-
-(for ([n (in-naturals)]
-      #:break (not (solver_NextSolution solver db)))
-  (displayln 
-    (format 
-      "Solution ~a:\n x=~a\n y=~a\n z=~a\n" 
-      n 
-      (value_IntVar x)
-      (value_IntVar y)
-      (value_IntVar z))))
-
